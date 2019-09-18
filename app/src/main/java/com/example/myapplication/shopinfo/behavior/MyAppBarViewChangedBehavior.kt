380e3183653f3d2120f0838069dcb1ccf95d4bde
XiaoGui <1057415595@qq.com>
2019-09-18 10:38:09 +0800

    更改代码，使计算偏移的准确性更高

--- app/src/main/java/com/example/myapplication/shopinfo/behavior/MyAppBarViewChangedBehavior.kt
+++ app/src/main/java/com/example/myapplication/shopinfo/behavior/MyAppBarViewChangedBehavior.kt
@@ -11,6 +11,7 @@ import androidx.core.widget.NestedScrollView
 import com.example.myapplication.utils.LogUtils
 import com.google.android.material.appbar.AppBarLayout
 import java.security.AlgorithmParameterGenerator
+import kotlin.math.abs
 
 class MyAppBarViewChangedBehavior: AppBarLayout.Behavior {
     val ALPHA_VIEWGROUP = "floating_alpha_layout"
@@ -42,41 +43,40 @@ class MyAppBarViewChangedBehavior: AppBarLayout.Behavior {
             scrollViewCriticalY = toolbar?.y!! + toolbar?.height!!+tabLayout?.height!!
         }
 
-
-//        nestedScrollView?.addOnLayoutChangeListener { v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom ->
-//            var curY = nestedScrollView!!.y
-//            var alpha = (scrollRawY-curY)/(scrollRawY-scrollViewCriticalY)
-//            LogUtils.e("alpha: $alpha")
-//            LogUtils.e("curY: $curY")
-//            alphaViewGroup?.alpha = alpha
-//            alphaViewGroup?.invalidate()
-//        }
         abl.addOnOffsetChangedListener(object: AppBarLayout.OnOffsetChangedListener{
             override fun onOffsetChanged(p0: AppBarLayout?, p1: Int) {
+
+                /**
+                 * 此段垃圾代码不可用
                 if(scrollRawY ==0F)scrollRawY=nestedScrollView!!.y
                 var curY = nestedScrollView!!.y
                 var alpha = (scrollRawY-curY)/(scrollRawY-scrollViewCriticalY)
                 if(curY-scrollViewCriticalY<20)alpha = 1F
                 if(scrollRawY-curY<20)alpha = 0F
-                LogUtils.e("scrollRawY: $scrollRawY")
-                LogUtils.e("scrollViewCriticalY: $scrollViewCriticalY")
-                LogUtils.e("curY: $curY")
+                LogUtils.e("scrollRawY: $scrollRawY" )
+                LogUtils.e("scrollViewCriticalY: $scrollViewCriticalY" )
+                LogUtils.e("curY: $curY" )
                 LogUtils.e("alpha: $alpha")
                 alphaViewGroup?.alpha = alpha
                 alphaViewGroup?.invalidate()
                 ivCircle?.alpha = 1-alpha
                 ivCircle?.invalidate()
+                 */
+                //使用 abl.totalScrollRange 方法的实时性 比 获取 nestedScrollView 的y 值 要好！！
+                LogUtils.e("p1: $p1 \np0.totalScrollRange: ${p0?.totalScrollRange} \nabs(p1): ${abs(p1)}\nabs(p1).toFloat: ${abs(p1).toFloat()}\np0.totalScrollRnage.toFloat: ${p0?.totalScrollRange!!.toFloat()}")
+                LogUtils.e("alpha2: ${abs(p1).toFloat()/p0?.totalScrollRange!!.toFloat()}")
+                var alpha = abs(p1).toFloat()/p0?.totalScrollRange!!.toFloat()
+                alphaViewGroup?.alpha = alpha
+                ivCircle?.alpha = 1- alpha
             }
-
         })
 
         LogUtils.e("toolbar.height: ${toolbar?.height}")
         LogUtils.e("alphaViewGroup.height: ${alphaViewGroup?.height}")
         LogUtils.e("scrollRawY: $scrollRawY")
         LogUtils.e("scrollViewCriticalY: $scrollViewCriticalY")
+        LogUtils.e("appbar getTotalScrollRange: ${abl.totalScrollRange}")   //获得abl可以滑动的最大偏移
 
         return handled
     }
-
-
 }
